693f0bb8508ba8f51a9c06ed8b0108b7c32e16eb
Ales Dostal <a.dostal@apitree.cz>
2018-05-11 21:57:19 +0200

    Add variables to Docker Add healtz

--- Dockerfile
+++ Dockerfile
@@ -16,5 +16,14 @@ RUN npm install
 # Build application, because Next.js have random HASH and BULD_ID
 RUN cd /usr/src/app && npm run build
 
+# Add arguments to process.env from docker build --build-arg xx
+ARG BUILD_NUM
+ENV BUILD_NUM=$BUILD_NUM
+
+ARG BUILD_AUTHOR
+ENV BUILD_AUTHOR=$BUILD_AUTHOR
+
+ENV TZ=Europe/Prague
+
 EXPOSE 8080
 CMD ["npm", "start"]
--- server/index.ts
+++ server/index.ts
@@ -9,6 +9,33 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
     const server = express();
 
+    server.get('/healthz', (_, res) => {
+        // check my health
+        // -> return next(new Error('DB is unreachable'))
+        res.sendStatus(200);
+    });
+
+    server.get('/_info', (_, res) => {
+        const {NODE_ENV, NODE_VERSION, LC_CTYPE, BACKEND_ENDPOINT, BUILD_AUTHOR, BUILD_NUM, BUILD_DATE, K8S_NAMESPACE} = process.env;
+        const {name, version, description, author, homepage, dependencies} = require('../package.json');
+        res.json({
+            NAME: name,
+            DESCRIPTION: description,
+            AUTHOR: author,
+            VERSION: version,
+            HOMEPAGE: homepage,
+            BACKEND_ENDPOINT,
+            NODE_ENV,
+            NODE_VERSION,
+            LC_CTYPE,
+            BUILD_AUTHOR,
+            BUILD_NUM,
+            BUILD_DATE,
+            K8S_NAMESPACE,
+            DEPENDENCIES: dependencies,
+        });
+    });
+
     server.get('*', (req, res) => handle(req, res));
 
     const PORT = process.env.PORT || 8080;
