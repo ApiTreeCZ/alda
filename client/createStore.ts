601c3077da85c614599da70e8faffea65e1d983c
Ales Dostal <a.dostal@apitree.cz>
2018-05-12 22:20:07 +0200

    Update to alpha.18 Persist themeOptions to localStorage

--- client/createStore.ts
+++ client/createStore.ts
@@ -2,6 +2,7 @@ import {applyMiddleware, compose, createStore as reduxCreateStore, StoreCreator}
 import thunkMiddleware from 'redux-thunk';
 import {Store} from './Store';
 import {rootReducer} from './rootReducer';
+import {ThemeOptions} from 'material-ui/styles/createMuiTheme';
 
 declare const window: any;
 
@@ -9,5 +10,29 @@ const isEnableReduxDevTools = (isServer: boolean) => !isServer && window.__REDUX
 
 const reduxTools = (store: StoreCreator, isServer: boolean) => (isEnableReduxDevTools(isServer) ? window.__REDUX_DEVTOOLS_EXTENSION__()(store) : store);
 
-export const createStore = (initialState: Store, {isServer}: {isServer: boolean}) =>
-    compose(applyMiddleware(thunkMiddleware))(reduxTools(reduxCreateStore, isServer))(rootReducer, initialState);
+export const createStore = (initialState: Store, {isServer}: {isServer: boolean}) => {
+    const state = isServer
+        ? initialState
+        : {...initialState, page: {...initialState.page, themeOptions: {...initialState.page.themeOptions, ...getThemeOptions()}}};
+    const store = compose(applyMiddleware(thunkMiddleware))(reduxTools(reduxCreateStore, isServer))(rootReducer, state);
+    store.subscribe(() => {
+        saveThemeOptions(store.getState().page.themeOptions);
+    });
+    return store;
+};
+
+const getThemeOptions = (): ThemeOptions | undefined => {
+    try {
+        const find = localStorage.getItem('themeOptions');
+        if (find === null) {
+            return undefined;
+        }
+        return JSON.parse(find);
+    } catch (err) {
+        return undefined;
+    }
+};
+
+const saveThemeOptions = (theme: ThemeOptions) => {
+    localStorage.setItem('themeOptions', JSON.stringify(theme));
+};
